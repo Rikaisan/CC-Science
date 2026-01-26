ffc026321e22b9afaa0f9971bbc642d84c0e4b3d
Rikai <rikaisensei@gmail.com>
2026-01-26 14:46:44 -0500

    refactor(Main): Fix inconsistent spacing

--- src/main/java/com/rikaisan/ccscience/CCScience.java
+++ src/main/java/com/rikaisan/ccscience/CCScience.java
@@ -11,21 +11,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CCScience implements ModInitializer {
-	public static final String MOD_ID = "cc-science";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "cc-science";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-                LOGGER.info("Loaded CC: Science!");
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Loaded CC: Science!");
 
-                ModItems.initialize();
-                ModBlocks.initialize();
-                CCScienceBlockEntityType.initialize();
+        ModItems.initialize();
+        ModBlocks.initialize();
+        CCScienceBlockEntityType.initialize();
 
-                registerPeripherals();
-	}
+        registerPeripherals();
+    }
 
-        private void registerPeripherals() {
-                PeripheralLookup.get().registerForBlockEntity((blockEntity, direction) -> new EntityRadarPeripheral(blockEntity), CCScienceBlockEntityType.ENTITY_RADAR);
-        }
+    private void registerPeripherals() {
+        PeripheralLookup.get().registerForBlockEntity((blockEntity, direction) -> new EntityRadarPeripheral(blockEntity), CCScienceBlockEntityType.ENTITY_RADAR);
+    }
 }
