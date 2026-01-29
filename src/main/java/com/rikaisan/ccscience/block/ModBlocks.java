e29bd7b77ffddbe2377ef3d4d680b933463510fe
Rikai <rikaisensei@gmail.com>
2026-01-28 19:15:26 -0500

    refactor(ModBlocks): Remove unused imports

--- src/main/java/com/rikaisan/ccscience/block/ModBlocks.java
+++ src/main/java/com/rikaisan/ccscience/block/ModBlocks.java
@@ -1,9 +1,7 @@
 package com.rikaisan.ccscience.block;
 
 import com.rikaisan.ccscience.CCScience;
-import com.rikaisan.ccscience.item.ModItems;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -16,9 +14,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 public class ModBlocks {
     public static final Block ENTITY_RADAR = register("entity_radar", new EntityRadarBlock(Properties.of().sound(SoundType.STONE).strength(1.0f)), true);
 
-    public static void initialize() {
-
-    }
+    public static void initialize() {}
 
 	public static Block register(String name, Block block, boolean shouldRegisterItem) {
 		ResourceLocation id = ResourceLocation.fromNamespaceAndPath(CCScience.MOD_ID, name);
